f28ab0770f18eefdbf78aca03b4a873609ebe542
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-12-22 11:24:26 +0530

    Add files via upload

---
+++ Hook/useContext/src/Hindi.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hindi() {
+    return (
+        <div style={{margin:"10px"}}>
+            <h1>शक्ति सिंह चूंडावत</h1>
+            <h1>मैं रिएक्ट डेवलपर हूं</h1>
+        </div>
+    )
+}
