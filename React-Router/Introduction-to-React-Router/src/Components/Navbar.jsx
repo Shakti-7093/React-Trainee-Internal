2404b21c828709d7781ee9dcb48b86102390d068
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-12-29 18:12:09 +0530

    Update Navbar.jsx

--- React-Router/Introduction-to-React-Router/src/Components/Navbar.jsx
+++ React-Router/Introduction-to-React-Router/src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -6,13 +7,13 @@ function Navbar() {
         <nav>
             <ul>
                 <li>
-                    <a href="/">Home</a>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <a href="/about">About</a>
+                    <Link to="/about">About</Link>
                 </li>
                 <li>
-                    <a href="/contact">Contact</a>
+                    <Link to="/contact">Contact</Link>
                 </li>
             </ul>
         </nav>
