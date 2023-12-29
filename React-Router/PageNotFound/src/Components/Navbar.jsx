26add12d1c5cbe90d51bc88d287668074c12bb81
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-12-29 18:13:53 +0530

    Update Navbar.jsx

--- React-Router/PageNotFound/src/Components/Navbar.jsx
+++ React-Router/PageNotFound/src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -6,13 +7,13 @@ function Navbar() {
       <nav>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/other">Incorrect URL</a>
+            <Link to="/other">Incorrect URL</Link>
           </li>
         </ul>
       </nav>
