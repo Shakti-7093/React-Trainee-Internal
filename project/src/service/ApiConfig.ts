d0bbd00069bcbf05ebe3867cc57846767a1d44d6
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2024-01-21 17:02:51 +0530

    Add files via upload

---
+++ project/src/service/ApiConfig.ts
@@ -0,0 +1,8 @@
+// const baseUrl = "https://bv6gp6b8-3000.inc1.devtunnels.ms/";
+const baseUrl = "http://localhost:3000/"
+
+const ApiConfig = {
+  user: `${baseUrl}user`,
+};
+
+export default ApiConfig;
